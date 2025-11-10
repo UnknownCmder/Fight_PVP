2a80e51970b9e85a47ccf113a1079ff188f55a1b
chlwns1020 <chy3022704@gmail.com>
2025-11-11 00:18:43 +0900

    Sniper 탄속, 데미지 증가량 조정

--- item/Sniper.py
+++ item/Sniper.py
@@ -5,12 +5,12 @@ from Tool import secondToTick
 
 class Sniper(Gun):
     def __init__(self, image, position: pg.Vector2):
-        super().__init__(image, position, size=80, bullet_speed=40)
-        self.init_attack_cooltime = secondToTick(1)
+        super().__init__(image, position, size=(80, 80), bullet_speed=60)
+        self.init_attack_cooltime = secondToTick(1) #총 발사 쿨타임
 
     def shoot(self, shooter):
         image = pg.image.load("./assets/bullet.png").convert_alpha()
         image = pg.transform.rotate(image, -self.angle)
 
         from map.CreateMap import bullets
-        bullets.add(Bullet(image, self.position, 20, pg.Vector2(0, self.bullet_speed).rotate(self.angle), shooter, 1, 0.15))
+        bullets.add(Bullet(image, self.position, 20, pg.Vector2(0, self.bullet_speed).rotate(self.angle), shooter, 1, 3))
